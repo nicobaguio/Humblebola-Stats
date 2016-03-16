e28963883a14957be5d5cf366e5b8f4c7b236d9a
Nico Baguio <nicobaguio41@gmail.com>
2016-03-16 04:43:16 +0000

    Refactor league and team links on base.html

--- humblebola/urls.py
+++ humblebola/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page, name="home_page"),
     url(r'games/$', views.schedule, name="schedule"),
-    url(r'teams/(?P<team_code>\w+)/', views.team_home_page, name="team_home_page"),
+    url(r'teams/(?P<team_code>[\w-]+)/', views.team_home_page, name="team_home_page"),
 ]
